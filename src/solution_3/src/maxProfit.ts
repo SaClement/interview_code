931ca366fda49d211fefdf228126e64e35e63cca
SaClement <15213075743@163.com>
2022-07-16 19:41:53 +0800

    edit

--- src/solution_3/src/maxProfit.ts
+++ src/solution_3/src/maxProfit.ts
@@ -1,5 +1,5 @@
 // 最大收益计算
-function maxProfit(prices: Array<number>) {
+export function maxProfit(prices: Array<number>) {
   let maxprofit: number = 0;
   for (let i = 0; i < prices.length - 1; i++) {
     for (let j = i + 1; j < prices.length; j++) {
@@ -12,15 +12,14 @@ function maxProfit(prices: Array<number>) {
   return maxprofit;
 }
 
-
 /**
  * 一次遍历
  * @param prices 价格
- * @returns 最大收益 
+ * @returns 最大收益
  */
-function oneMaxProfit(prices: Array<number>) {
+export function oneMaxProfit(prices: Array<number>) {
   let minprice: number = Infinity;
-  let maxprofit:number = 0;
+  let maxprofit: number = 0;
   for (let i = 0; i < prices.length; i++) {
     if (prices[i] < minprice) {
       minprice = prices[i];
