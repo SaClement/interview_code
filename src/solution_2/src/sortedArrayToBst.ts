57c6a1478dcdd90bc5f089d82d8c51e0e8ca3687
SaClement <15213075743@163.com>
2022-07-22 12:25:32 +0800

    BST

--- src/solution_2/src/sortedArrayToBst.ts
+++ src/solution_2/src/sortedArrayToBst.ts
@@ -1,12 +1,12 @@
 import { TreeNode } from "../../utils";
 
-export function sortedArrayToBST(nums: Array<number>): TreeNode<T> | null {
+export function sortedArrayToBST(nums: Array<number>): TreeNode<number> | null {
   // 如果数组长度为0
   if (nums.length === 0) {
     return null;
   }
 
-  return <TreeNode<T>>helper(nums, 0, nums.length - 1);
+  return <TreeNode<number>>helper(nums, 0, nums.length - 1);
 }
 
 /**
@@ -16,16 +16,12 @@ export function sortedArrayToBST(nums: Array<number>): TreeNode<T> | null {
  * @param right 右节点
  * @returns
  */
-function helper(
-  nums: Array<number>,
-  left: number,
-  right: number
-): TreeNode<T>{
+function helper(nums: Array<number>, left: number, right: number): TreeNode<number> {
   // 以中间位置左边的数为根节点
   const mid = (left + right) / 2;
 
   const root = new TreeNode(nums[mid]);
-  root.left = <TreeNode<T>>helper(nums, left, mid - 1);
-  root.right = <TreeNode<T>>helper(nums, mid + 1, right);
+  root.left = <TreeNode<number>>helper(nums, left, mid - 1);
+  root.right = <TreeNode<number>>helper(nums, mid + 1, right);
   return root;
 }
